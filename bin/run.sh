4397bcd05492f021a261df354bdf44359ba291e3
Gergely Mentsik <gergely@mentsik.eu>
2017-01-13 19:27:41 +0100

    Alpine crond should work now

--- bin/run.sh
+++ bin/run.sh
@@ -7,8 +7,8 @@ cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 echo "${TIMEZONE}" >  /etc/timezone
 
 echo "Registering new cron..."
-echo "${CRON} root echo \"Hello world\" >> /var/log/cron.log 2>&1 \n" >> /etc/cron.d/docktartar
-chmod 0644 etc/cron.d/docktartar
+echo "${CRON} echo \"Hello world\" >> /var/log/cron.log 2>&1 \n" >> /root/docktartar.cron
+crontab /root/docktartar.cron
 
 echo "OK - waiting for job to execute."
-cron && tail -f /var/log/cron.log
+/usr/sbin/crond && tail -f /var/log/cron.log
