0a15f36b73bd39b95fc8bee26304086e54e271a3
Gergely Mentsik <gergely@mentsik.eu>
2017-01-13 19:50:05 +0100

    Fixed wrong cron command

--- bin/run.sh
+++ bin/run.sh
@@ -7,7 +7,7 @@ cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 echo "${TIMEZONE}" >  /etc/timezone
 
 echo "Registering new cron..."
-echo "${CRON} echo \"Hello world\" >> /var/log/cron.log 2>&1 \n" >> /root/docktartar.cron
+echo "${CRON} /root/docktartar.sh >> /var/log/cron.log 2>&1 \n" >> /root/docktartar.cron
 crontab /root/docktartar.cron
 
 echo "OK - waiting for job to execute."
