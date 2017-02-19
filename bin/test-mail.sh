f820698cf05ce114e6ef594ec6f4dc4e428a886e
Gergely Mentsik <gergely@mentsik.eu>
2017-02-19 19:06:29 +0100

    Fixed spelling

--- bin/test-mail.sh
+++ bin/test-mail.sh
@@ -2,7 +2,7 @@
 echo "Sending test email"
 echo "From: ${EMAIL_FROM} <${EMAIL_FROM_ADRESS}>" > testmail.mail
 echo "Subject: ${EMAIL_SUBJECT}" >> testmail.mail
-echo "Your e-mail settings seems to work fine!" >> testmail.mail
+echo "Your e-mail settings seem to work fine!" >> testmail.mail
 ssmtp ${EMAIL_TO} < testmail.mail
 
 if [[ $? != 0 ]]; then
